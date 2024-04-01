05dcb162a8c87f4c83d3a411236d746bef0f0564
ksks2211 <rival15@naver.com>
2024-04-01 15:45:26 +0900

    Update Secret Managing Logic

--- src/main/java/org/iptime/yoon/blog/common/config/SecretsManagerEnvironmentPostProcessor.java
+++ src/main/java/org/iptime/yoon/blog/common/config/SecretsManagerEnvironmentPostProcessor.java
@@ -1,6 +1,7 @@
 package org.iptime.yoon.blog.common.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
@@ -18,6 +19,8 @@ import java.util.Arrays;
 public class SecretsManagerEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private static final String DEPLOYMENT_PROFILE = "deploy";
+
+
 
 
     @Override
@@ -42,53 +45,12 @@ public class SecretsManagerEnvironmentPostProcessor implements EnvironmentPostPr
             // Assuming the secret value is a JSON string with a key named 'message'
             ObjectMapper mapper = new ObjectMapper();
 
+            JsonNode jsonNode = mapper.readTree(secretValue);
 
-
-            String value = mapper.readTree(secretValue).get("spring.datasource.url").asText();
-            System.setProperty("spring.datasource.url", value);
-
-
-            value = mapper.readTree(secretValue).get("spring.datasource.username").asText();
-            System.setProperty("spring.datasource.username", value);
-
-            value = mapper.readTree(secretValue).get("spring.datasource.password").asText();
-            System.setProperty("spring.datasource.password", value);
-
-            value = mapper.readTree(secretValue).get("spring.security.oauth2.client.registration.google.client-id").asText();
-            System.setProperty("spring.security.oauth2.client.registration.google.client-id", value);
-            value = mapper.readTree(secretValue).get("spring.security.oauth2.client.registration.google.client-secret").asText();
-            System.setProperty("spring.security.oauth2.client.registration.google.client-secret", value);
-
-            value = mapper.readTree(secretValue).get("spring.security.oauth2.client.registration.google.redirect-uri").asText();
-            System.setProperty("spring.security.oauth2.client.registration.google.redirect-uri", value);
-
-
-            value = mapper.readTree(secretValue).get("spring.data.redis.host").asText();
-            System.setProperty("spring.data.redis.host", value);
-
-
-
-            value = mapper.readTree(secretValue).get("cloud.aws.region.static").asText();
-            System.setProperty("cloud.aws.region.static", value);
-
-
-            value = mapper.readTree(secretValue).get("cloud.aws.s3.bucket").asText();
-            System.setProperty("cloud.aws.s3.bucket", value);
-
-
-            value = mapper.readTree(secretValue).get("cdn.baseUrl").asText();
-            System.setProperty("cdn.baseUrl", value);
-
-            value = mapper.readTree(secretValue).get("cors.allowed").asText();
-            System.setProperty("cors.allowed", value);
-
-
-
-            value = mapper.readTree(secretValue).get("auth.jwt.secret-key").asText();
-            System.setProperty("auth.jwt.secret-key", value);
-
-            value = mapper.readTree(secretValue).get("auth.jwt.issuer").asText();
-            System.setProperty("auth.jwt.issuer", value);
+            jsonNode.fieldNames().forEachRemaining(key->{
+                String value = jsonNode.get(key).asText();
+                System.setProperty(key,value);
+            });
 
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
